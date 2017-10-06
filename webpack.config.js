d6e36a9c3c766f4373438562d03548fa7156b6e9
Peter Bedor <peterbedor@gmail.com>
2017-10-06 15:21:21 -0500

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './scripts/woo.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'public/js'),
+		filename: '[name].bundle.js'
+	},
+	devServer: {
+		contentBase: './public'
+	},
+	module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015']
+                 }
+             }
+         ]
+     },
+	stats: {
+		colors: true
+	},
+	watch: true
+};
