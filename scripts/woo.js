5240d974f7c56efafbc6b9629ee128297ec46a47
Peter Bedor <peterbedor@gmail.com>
2017-10-06 21:40:50 -0500

    Resolve merge issues

--- scripts/woo.js
+++ scripts/woo.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log('hi');
-=======
 console.log('hi!!!');
->>>>>>> e0d8f5ce0577a65b1f0ee86d0d62ffa5536fe56b
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-<<<<<<< HEAD
 	entry: {
 		app: './scripts/woo.js'
 	},
@@ -14,20 +13,7 @@ module.exports = {
 	devServer: {
 		contentBase: './public'
 	},
-	module: {
-=======
-    entry: {
-		app: './scripts/woo.js'
-	},
-    output: {
-        path: path.resolve(__dirname, 'public/js'),
-        filename: '[name].bundle.js'
-    },
-	devServer: {
-		contentBase: './public'
-	},
     module: {
->>>>>>> e0d8f5ce0577a65b1f0ee86d0d62ffa5536fe56b
          loaders: [
              {
                  test: /\.js$/,
@@ -38,16 +24,8 @@ module.exports = {
              }
          ]
      },
-<<<<<<< HEAD
 	stats: {
 		colors: true
 	},
 	watch: true
 };
-=======
-     stats: {
-         colors: true
-     },
-	 watch: true
-};
->>>>>>> e0d8f5ce0577a65b1f0ee86d0d62ffa5536fe56b
